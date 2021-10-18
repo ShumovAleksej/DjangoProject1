149baf6bd5f45e730380efd5d3090630229a42f9
ShumovAleksej <45117792+ShumovAleksej@users.noreply.github.com>
2021-10-19 00:42:43 +0500

    settings.py

--- realshop/settings.py
+++ realshop/settings.py
@@ -19,12 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m61bbw(-9_dn*5!a0w8b6h)__!38e55hg@=2+(#0!dm$1hg#%('
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
